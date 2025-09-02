b2a51bf5236a1fa8798cff2cda935674f5b259bf
Nicolle Q <nicollejacome008@gmail.com>
2025-09-02 15:36:21 -0500

    Table html

---
+++ config/database_backup/table_users.sql
@@ -0,0 +1,32 @@
+create table users( 
+ id bigserial primary key,
+ first name varchar (30) not null,
+ last name varchar(30) not null,
+ mobile_number varchar (20) not null,
+ ide_number varchar(15) noy null unique,
+ address text null,
+ birhdate date null,
+ email varchar (200) not null unique,
+ password text not null,
+ status boolean not null default true,
+ created_at timestamptz not null default now(),
+ update_at timestamptz not null default now(),
+ deleted_at timestamptz null
+);
+
+--insert into table
+INSERT INTO users(
+firstname, 
+lastname, 
+mobile_number,
+email,
+password
+);
+
+VALUES (
+'Nicolle',
+'Quijano',
+'3184830987',
+'nico@mail.com',
+'1234'
+);
